14752835ab1c52df8d36440258eda29b3f0b99b8
Abegail Pates <abegail.pates@gmail.com>
2022-10-15 17:58:42 +0800

    Add way to output book array contents

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const cardContainer = document.getElementById('card-container');
 let myLibrary = [];
 
 function Book(title, author, pages, isRead) {
@@ -11,11 +12,32 @@ function addBookToLibrary(title, author, pages, isRead) {
     myLibrary.push(new Book(title, author, pages, isRead));
 }
 
-let title = 'Yangchen';
-let author = 'Yee';
-let pages = 300;
-let isRead = true;
-
-addBookToLibrary(title, author, pages, isRead);
-
+addBookToLibrary('Yangchen', 'Yee', 300, true);
+addBookToLibrary('Harry Potter', 'sumbitch', 1000, false);
 console.log(myLibrary);
+
+myLibrary.forEach(book => {
+    console.log(book.title);
+
+    const card = document.createElement('div');
+    cardContainer.append(card);
+
+    const title = document.createElement('div');
+    const author = document.createElement('div');
+    const pages = document.createElement('div');
+    const isRead = document.createElement('div');
+
+    title.textContent = book.title;
+    author.textContent = 'by ' + book.author;
+    pages.textContent = book.pages + ' pages';
+    if(book.isRead === true) {
+        isRead.textContent = 'Read';
+    } else {
+        isRead.textContent = 'Unread';
+    }
+
+    card.append(title);
+    card.append(author);
+    card.append(pages);
+    card.append(isRead);
+})
